5ae524bf6a3f3a4e6393343ae6d7adaf62d7fda1
shluna <santiagohluna@gmail.com>
2024-06-04 20:36:20 -0300

    Primer commit

---
+++ src/paleo_martesol.f90
@@ -0,0 +1,10 @@
+program paleo_martesol
+
+    use paleo_martesol_subs
+
+    implicit none
+
+!   Propósito
+    call calcular_posicion_sol()
+    
+end program paleo_martesol
---
+++ src/paleo_martesol_subs.f90
@@ -0,0 +1,15 @@
+module paleo_martesol_subs
+
+    implicit none
+    
+contains
+
+    subroutine calcular_posicion_sol()
+
+        implicit none
+
+        
+
+    end subroutine
+    
+end module paleo_martesol_subs
