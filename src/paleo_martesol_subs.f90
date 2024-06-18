9dce02877646dfa27229e571b1aea0559e19c41e
Santiago Luna <santiagohluna@gmail.com>
2024-06-18 18:53:15 -0300

    Se modificó la subrutina que lee el DEM y la declaración de algunas variables y arreglos

--- src/paleo_martesol_subs.f90
+++ src/paleo_martesol_subs.f90
@@ -4,14 +4,18 @@ module paleo_martesol_subs
 
     implicit none
 
+    integer :: kmax
+
     real(dp), parameter :: pi = 4.d0*datan(1.d0)
     real(dp), parameter :: radm = 3396.19d0
     real(dp), parameter :: uakm = 149597870.7d0
     real(dp), parameter :: epse = 23.43928d0*pi/180.d0
 
     real(dp) :: t0,dt,fdt
+
+    real(dp), allocatable, dimension(:) :: colat,elong,z
     
-contains
+    contains
 
     subroutine calcular_posicion_sol()
 
@@ -19,15 +23,13 @@ contains
 
         character(len=100) :: filein,pathout
 
-        real(dp), dimension(10000000) :: colat,elong,z
-
         ! Estrategia
 
         call leer_archivo_entrada(filein,pathout)
 
-        call leer_archivo_dem(filein,colat,elong,z)
+        call leer_archivo_dem(filein)
 
-        call procesar_datos_escribir_salida()
+        ! call procesar_datos_escribir_salida(pathout)
 
     end subroutine
 
@@ -54,67 +56,81 @@ contains
         
     end subroutine leer_archivo_entrada
 
-    subroutine leer_archivo_dem(filein,colat,elong,z)
+    subroutine leer_archivo_dem(filein)
         
         implicit none
 
-        integer :: k,feof,kmax
+        integer :: k,feof
 
         real(dp) :: long,lat,h
-
-        real(dp), dimension(:),intent(out) :: colat,elong,z
 
         character(len=100),intent(in) :: filein
 
         open(unit=11,file=trim(filein))
 
         k = 1
-
         
         feof = 0
         
-        print *, 'procesando archivo "'//trim(filein)//'"'
+        print *, 'Procesando archivo "'//trim(filein)//'"'
 
         do while(feof.eq.0)
 
-         read(11,*,iostat=feof) long,lat,h
+            read(11,*,iostat=feof) long,lat,h
 
-         if (feof.gt.0) then
-          print *,'revisar archivo de entrada'
-          exit
-         else if (feof.lt.0) then
-          print *,  'listo!'
-          exit
-         else
-
-          if (lat.ge.0.d0) then
-           colat(k) = 90.d0 - lat
-          else
-           colat(k) = 90.d0 + dabs(lat)
-          end if
-
-          if (long.ge.0.d0) then
-           elong(k) = long
-          else
-           elong(k) = 360.d0 - dabs(long)
-          end if
-
-          colat(k) = colat(k)*pi/180.d0
-          elong(k) = elong(k)*pi/180.d0
-              z(k) = h*1.d-3
-
-         end if
-         k = k + 1
+            if (feof.gt.0) then
+                print *,'Revisar archivo de entrada'
+                exit
+            else if (feof.lt.0) then
+                print *,  '¡Listo!'
+                exit
+            end if
+        
+            k = k + 1
+        
         end do 
+
+        close(11)
+        
         kmax = k - 1
+
+        allocate(colat(kmax), source=0.d0)
+        allocate(elong(kmax), source=0.d0)
+        allocate(z(kmax), source=0.d0)
+
+        open(unit=11,file=trim(filein))
+
+        do k=1,kmax
+
+            read(11,*,iostat=feof) long,lat,h
+
+            colat(k) = 90.d0 - lat
+
+            if (long.ge.0.d0) then
+                elong(k) = long
+            else
+                elong(k) = 360.d0 + long
+            end if
+
+            colat(k) = colat(k)*pi/180.d0
+            elong(k) = elong(k)*pi/180.d0
+            z(k) = h*1.d-3
+
+        end do
+
+        do k=1,10,1
+            print *,colat(k),elong(k),z(k)
+        end do
         
     end subroutine leer_archivo_dem
 
-    subroutine procesar_datos_escribir_salida()
+    subroutine procesar_datos_escribir_salida(pathout)
 
         implicit none
 
+        character(len=100), intent(in) :: pathout
         
+        open(unit=12,file=trim(pathout))
         
     end subroutine procesar_datos_escribir_salida
     
