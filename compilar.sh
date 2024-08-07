a7874fe260d5b0425818831d682946caff003c02
Santiago Luna <santiagohluna@gmail.com>
2024-08-07 14:22:56 -0300

    Se modificó el script compilar.sh para que cree la carpeta bin y deje ahí los ejecutables

--- compilar.sh
+++ compilar.sh
@@ -2,6 +2,10 @@
 
 echo "Compilando."
 
+mkdir bin
+
+cd bin
+
 gfortran -g -fbacktrace -fcheck="all" -Wall -o paleomartesol.x ../src/paleo_martesol.f90 ../src/paleo_martesol_subs.f90
 
 echo "¡Listo!"
