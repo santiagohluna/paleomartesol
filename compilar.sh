d13ce50365e89325050acc3776035c0b91ffdd33
Santiago Luna <santiago.luna@unahur.edu.ar>
2024-08-28 15:25:58 -0300

    Se adecuó el archivo de compilación para la correcta visualización de la barra de progreso

--- compilar.sh
+++ compilar.sh
@@ -12,6 +12,6 @@ fi
 
 cd bin
 
-gfortran -g -fbacktrace -fcheck="all" -Wall -o paleomartesol.x ../src/paleo_martesol.f90 ../src/paleo_martesol_subs.f90
+gfortran -g -fbacktrace -fcheck="all" -Wall -fbackslash -o paleomartesol.x ../src/paleo_martesol.f90 ../src/paleo_martesol_subs.f90
 
 echo "¡Listo!"
